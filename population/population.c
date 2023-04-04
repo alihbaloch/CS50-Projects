1bbd4044064533f5467e8b17ac758e79f300ad4f
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-04 20:14:58 +0000

    Tue, Apr 4, 2023, 4:14 PM -04:00

---
+++ population/population.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+    string first_name = get_string("What is first your name?: ");
+    string last_name = get_string("What is your last name?: ");
+
+    printf("Hello, %s %s\n", first_name, last_name);
+
+    // TODO: Prompt for end size
+
+    // TODO: Calculate number of years until we reach threshold
+
+    // TODO: Print number of years
+}
