ac1cef8a0977590a39a0e856035bf27299155984
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-04 21:20:26 +0000

    Tue, Apr 4, 2023, 5:20 PM -04:00

--- population/population.c
+++ population/population.c
@@ -12,6 +12,7 @@ int start_population = get_int("What is starting population?: ");
 while (start_population >= 9);
     // TODO: Prompt for end size
 int end_population = get_int("What is ending population?: ");
+while (end_population <)
 
     // TODO: Calculate number of years until we reach threshold
 
