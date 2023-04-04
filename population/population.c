045cf1f7fe681200c2cbccbfe08b3bb937bf3d97
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-04 20:31:50 +0000

    Tue, Apr 4, 2023, 4:31 PM -04:00

--- population/population.c
+++ population/population.c
@@ -4,10 +4,6 @@
 int main(void)
 {
     // TODO: Prompt for start size
-    string first_name = get_string("What is first your name?: ");
-    string last_name = get_string("What is your last name?: ");
-
-    printf("Hello, %s %s\n", first_name, last_name);
 
     // TODO: Prompt for end size
 
