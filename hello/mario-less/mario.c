cc335f2bac9e877c7f83e78ec9ed3dfbcbab3db6
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-05 22:45:37 +0000

    Wed, Apr 5, 2023, 6:45 PM -04:00

--- hello/mario-less/mario.c
+++ hello/mario-less/mario.c
@@ -11,17 +11,18 @@ int main(void)
     while (block < 1 || block > 8);
 
     for (block = 0; block < 8; block++)
+        for (int block_2 = 0; block_2 < 8; block_2++)
+
         {
             {
                 printf("#\n");
             }
-        }
 
-        for (int block_2 = 0; block_2 < 8; block_2++)
             {
                 printf("#");
             }
+        }
 
-            printf("\n");
+            
         }
 
