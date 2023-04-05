daf5ce0628fca5e9191faf534fb98ee131255cba
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-05 04:43:15 +0000

    Wed, Apr 5, 2023, 12:43 AM -04:00

--- hello/mario-less/mario.c
+++ hello/mario-less/mario.c
@@ -24,13 +24,13 @@ int main(void)
 
 //}
 {
-for (int i = 0; i < 3; i++)
-{
-    for (int j = 0; j < 3; j++)
+    for (int i = 0; i < 3; i++)
     {
-        printf("#");
+        for (int j = 0; j < 3; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
     }
-    printf("\n");
-}
 
 }
