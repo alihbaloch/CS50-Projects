2a766cf2d4732f6f73716fcf5a9617e7591896d0
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-05 22:35:38 +0000

    Wed, Apr 5, 2023, 6:35 PM -04:00

--- hello/mario-less/mario.c
+++ hello/mario-less/mario.c
@@ -16,11 +16,11 @@ int main(void)
             {
                 printf("#\n");
             }
-           // printf("\n");
 
             {
-            //printf("#");
+            printf("#");
             }
+
             //printf("\n");
         }
 
