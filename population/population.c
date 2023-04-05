bf73bb1706ceb92d620e3def14b50fe5dcf4dfd5
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-05 00:12:19 +0000

    Tue, Apr 4, 2023, 8:12 PM -04:00

--- population/population.c
+++ population/population.c
@@ -7,28 +7,28 @@ int main(void)
     int start_population;
 
     do
-    {
+    
         start_population = get_int("What is population start size?: ");
-    }
+
     while (start_population < 9);
     // TODO: Prompt for end size
 
     int end_population;
 
     do
-    {
+
         end_population = get_int("What is population end size?: ");
-    }
+
     while (end_population < start_population);
 
     int years = 0;
 
     // TODO: Calculate number of years until we reach threshold
     while (start_population < end_population)
-    {
+
         start_population = start_population + (start_population / 3) - (start_population / 4);
         years++;
-    }
+
     // TODO: Print number of years
 
     printf("Years: %i\n", years);
