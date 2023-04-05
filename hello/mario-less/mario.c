c08e7892568337d87cf9361b3e3d04976c52bc1d
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-05 01:00:56 +0000

    Tue, Apr 4, 2023, 9:00 PM -04:00

--- hello/mario-less/mario.c
+++ hello/mario-less/mario.c
@@ -3,8 +3,8 @@
 
 int main(void)
 {
-    int block = get_integar("Type a number between 1-8: ");
+    int block = get_int("Type a number between 1-8: ");
 
-    printf(#\n);
+    printf("#\n");
 
 }
