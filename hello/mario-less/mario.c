e5d7a957052a6dbd8e0c9ffbc047f4e3bd766d49
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-10 10:36:28 +0000

    Mon, Apr 10, 2023, 6:36 AM -04:00

--- hello/mario-less/mario.c
+++ hello/mario-less/mario.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    float dot = 3.0;
+    int space = 8;
     int height;
     do
     {
@@ -11,7 +11,7 @@ int main(void)
     }
     while (height < 1 || height > 8);
 
-    for (int block = 0; block < height; block++, dot++)
+    for (int block = 0; block < height; block++)
         {
             for (int block_2 = block; block_2 >= 0; block_2--)
                 {
