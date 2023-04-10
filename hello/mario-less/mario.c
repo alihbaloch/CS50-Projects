28abf13832b35f1311a7df6a2b9e86ed7287fe55
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-10 05:32:10 +0000

    Mon, Apr 10, 2023, 1:32 AM -04:00

--- hello/mario-less/mario.c
+++ hello/mario-less/mario.c
@@ -4,15 +4,16 @@
 int main(void)
 {
     int block;
+    const int n = 3;
     do
     {
         block = get_int("Enter pyramid height: ");
     }
     while (block < 1 || block > 8);
 
-    for (block = 0; block < 8; block++)
+    for (block = 0; block < n; block++)
         {
-            for (int block_2 = 0; block_2 < 8; block_2++)
+            for (int block_2 = 0; block_2 < n; block_2++)
                 {
                  printf("%i ", block_2);
                 }
