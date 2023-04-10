9123494a6b17b39185fefffd78b887bc633f28c7
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-10 02:41:19 +0000

    Sun, Apr 9, 2023, 10:41 PM -04:00

--- phonebook.c
+++ phonebook.c
@@ -7,13 +7,13 @@ int main(void)
     {
         string name = get_string("Enter your name: ");
         int age = get_int("Enter your age: ");
-        int phone = get_int("Enter your phone number: ");
+        long phone = get_long("Enter your phone number: ");
 
 
         {
             printf("Your name is %s\n", name);
             printf("Your age is %i\n", age);
-            printf("Your phone number is %i\n", phone);
+            printf("Your phone number is %li\n", phone);
         }
     }
 
