13d207a1dec28aea13e8313fbd17fb78b3d85a95
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-10 11:54:24 +0000

    Mon, Apr 10, 2023, 7:54 AM -04:00

--- hello/mario-less/mario.c
+++ hello/mario-less/mario.c
@@ -13,8 +13,8 @@ int main(void)
 
     for (int block = 0; block < height; block++)
         {
-            if block ==0 || block == 7
-            printf(" ");
+            if (block ==0 || block == 7, dot++)
+            printf("dot");
 
         for (int block_2 = block; block_2 >= 0; block_2--)
             {
