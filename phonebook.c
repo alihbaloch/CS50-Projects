1e9f3e991376e8f2e7bbd0d820272130e02fe723
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-10 02:18:51 +0000

    Sun, Apr 9, 2023, 10:18 PM -04:00

---
+++ phonebook.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <cs50.h>
+int main(void)
+
+string name = get_string("Enter your name: "):
+int age = get_int("Enter your age: ");
+int phone = get_int("Enter your phone number: ");
+
+printf("Your name is %s\n", name);
+printf("Your age is %i\n", age);
+printf("Your phone number is %i\n", phone);
