9c45069a1eaf9c7eee005b3fdca82becf0ba1971
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-11 08:20:02 +0000

    Tue, Apr 11, 2023, 4:20 AM -04:00

--- hello/mario-less/mario.c
+++ hello/mario-less/mario.c
@@ -10,11 +10,16 @@ int main(void)
         height = get_int("Enter pyramid height: ");
     }
     while (height < 1 || height > 8);
-    //
+
     for (int block = 0; block < height; block++)
+
+         // create a variable to print required spaces
         {
-        for (int dot = 6; dot >= block; dot--) //Understand this tomorrow. Copy to google docs and make notes. Change folder to its own. Check errors using check50 then submit.
+        for (int dot = 6; dot >= block; dot--) // Change folder to its own. Check errors using check50 then submit.
             printf(" ");
+
+
+            //create a variable to print hashes
 
         for (int block_2 = block; block_2 >= 0; block_2--)
             {
