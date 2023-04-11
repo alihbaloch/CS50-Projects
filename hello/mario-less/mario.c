2c69677eb2b89ce47fa20ff96f9c45a101fe88c2
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-11 11:39:08 +0000

    Tue, Apr 11, 2023, 7:39 AM -04:00

--- hello/mario-less/mario.c
+++ hello/mario-less/mario.c
@@ -15,7 +15,7 @@ int main(void)
 
          // create a variable to print required spaces
         {
-        for (int dot = 0; dot < block; dot++)
+        for (int dot = 0; dot < block; dot++) // problem lies here! Come back to it after lecture 2!
 
 
             printf(".");
