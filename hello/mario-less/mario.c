7b59ec0fa6aa5a53f2ff28657455f3012ef8e3c7
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-11 12:31:18 +0000

    Tue, Apr 11, 2023, 8:31 AM -04:00

--- hello/mario-less/mario.c
+++ hello/mario-less/mario.c
@@ -16,17 +16,17 @@ int main(void)
          // create a variable to print required spaces
         {
         for (int dot = 0; dot < block; dot++) // problem lies here! Come back to it after lecture 2!
+            printf("dot is %i\n", dot);
 
-
-            printf(".");
+            //printf(".");
 
             //create a variable to print hashes
 
         for (int block_2 = block; block_2 >= 0; block_2--)
             {
-                printf("#");
+                //printf("#");
             }
-                printf("\n");
+                //printf("\n");
         }
 
 }
