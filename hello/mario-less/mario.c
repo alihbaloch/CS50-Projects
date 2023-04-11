b5214f64e9d5e8364110b8a67d3e6cf9cdcf8188
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-11 10:56:38 +0000

    Tue, Apr 11, 2023, 6:56 AM -04:00

--- hello/mario-less/mario.c
+++ hello/mario-less/mario.c
@@ -15,7 +15,7 @@ int main(void)
 
          // create a variable to print required spaces
         {
-        for (int dot = height;  < 0; dot++)
+        for (int dot = 1; dot <= (8 - height); dot++)
 
             printf(".");
 
