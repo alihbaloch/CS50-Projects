e07cc484003c2b9c37f26da0a31f2406996d2cfd
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-14 04:13:48 +0000

    Fri, Apr 14, 2023, 12:13 AM -04:00

--- hello/mario-less/mario.c
+++ hello/mario-less/mario.c
@@ -15,13 +15,13 @@ int main(void)
 
          // create a variable to print required spaces
         {
-        for (int space = height - 1; space >= 1; space--) // problem lies here! Come back to it after lecture 2!
+        for (int block_2 = height - 1; block_2 >= 1; block_2--) // problem lies here! Come back to it after lecture 2!
 
             printf(".");
 
             //create a variable to print hashes
 
-        for (int block_2 = block; block_2 >= 0; block_2--) //check this one too, perhaps try using increments then go up print spaces
+        for (int block_2 = block; block_2 <= block; block_2++) //check this one too, perhaps try using increments then go up print spaces
             {
                 printf("#");
             }
