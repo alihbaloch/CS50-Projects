265082cc6a87c1fed8db949e2e6dbd2e5d677fe2
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-14 13:13:50 +0000

    Fri, Apr 14, 2023, 9:13 AM -04:00

--- credit/credit.c
+++ credit/credit.c
@@ -10,7 +10,7 @@ int main(void)
     {
         card_number = get_long("Enter credit card number: ");
     }
-    while(card_number == 4)
+    while(card_number == 4 && (length == 16))
     //prompt for input
 
 
