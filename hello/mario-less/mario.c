242ec6256b5cfaaee488b9bf31106554ad543c2f
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-14 04:27:55 +0000

    Fri, Apr 14, 2023, 12:27 AM -04:00

--- hello/mario-less/mario.c
+++ hello/mario-less/mario.c
@@ -21,7 +21,7 @@ int main(void)
 
             //print hashes
 
-        for (int block_2 = 0; block_2 <= block; block_2++) //check this one too, perhaps try using increments then go up print spaces
+        for (block_2 = 0; block_2 <= block; block_2++) //check this one too, perhaps try using increments then go up print spaces
             {
                 printf("#");
             }
