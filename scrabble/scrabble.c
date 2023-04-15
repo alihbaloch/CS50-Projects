3fe801727144580687d9a0e796fd84a97a355ed1
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-15 13:23:11 +0000

    Sat, Apr 15, 2023, 9:23 AM -04:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -19,10 +19,16 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
+    if (score1 > score2)
+        printf("Player 1 wins!\n");
+        elseif (score1 < score2)
+        printf("Player 2 wins!\n");
+        else
+        printf("Tie!\n");
 }
 
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
-    
+
 }
