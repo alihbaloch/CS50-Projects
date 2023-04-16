dde63ba3d6105a71e3ab9edc43e08241176d5993
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-16 11:09:01 +0000

    Sun, Apr 16, 2023, 7:09 AM -04:00

--- array.c
+++ array.c
@@ -13,13 +13,13 @@ int main (void)
         while (size < 1);
 
     int array[size];
-    array [0] = 1
+    array [0] = 1;
     printf("%i\n", array[0]);
 
     for (i = 1; i < size; i++)
     {
-        array{i} = 2 * array (i -1)
-        printf("%i\n", array{i});
+        array[i] = 2 * array (i - 1)
+        printf("%i\n", array[i]);
     }
 
 
