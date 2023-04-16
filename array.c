4ef709650f803ace585b8a727724c2e49efc3d58
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-16 12:32:11 +0000

    Sun, Apr 16, 2023, 8:32 AM -04:00

--- array.c
+++ array.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main (void)
 
+int main(void)
 {
-    int i;
-    int size;
-    do
-    {
-        size = get_int("Enter size: ");
-    }
-        while (size < 1);
+   int i;
+   int size;
+   do
+   {
+       size = get_int("Enter size: ");
+   }
+       while (size < 1);
 
-    int array[size];
-    array [0] = 1;
-    printf("%i\n", array[0]);
 
-    for (i = 1; i < size; i++)
-    {
-        array[i] = 2 * array [i -1];
-        printf("%i\n", array[i]);
-    }
+   int array[size];
+   array [0] = 1;
+   printf("%i\n", array[0]);
 
+
+   for (i = 1; i < size; i++)
+   {
+       array[i] = 2 * array [i -1];
+       printf("%i\n", array[i]);
+   }
 
 }
+
+
