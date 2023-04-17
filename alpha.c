51aeedfac7f5cdd4bb93f809130236401ac80aa0
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-17 08:14:56 +0000

    Mon, Apr 17, 2023, 4:14 AM -04:00

--- alpha.c
+++ alpha.c
@@ -1,12 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 
 {
     string word = get_string("Enter word: ");
-    
+    int length = strlen(word);
 
+    for(int i = 0; i < length; i++)
+    {
+        printf("%c ". length{i});
+    }
+
+    printf("\n");
 
 
 }
