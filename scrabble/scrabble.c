f6c018c1c4b02e849e0730d97ede0a98ee2f21a7
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-20 16:18:50 +0000

    Thu, Apr 20, 2023, 12:18 PM -04:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -20,11 +20,21 @@ int main(void)
 
     // TODO: Print the winner
     if (score1 > score2)
-        printf("Player 1 wins!\n");
+        {
+            printf("Player 1 wins!\n");
+        }
+
         elseif (score1 < score2)
-        printf("Player 2 wins!\n");
+
+        {
+            printf("Player 2 wins!\n");
+        }
+
         else
-        printf("Tie!\n");
+
+        {
+            printf("Tie!\n");
+        }
 }
 
 int compute_score(string word)
