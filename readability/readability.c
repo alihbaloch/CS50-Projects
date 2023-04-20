f831074a08105f6b9e19d7b128430349769cb3d5
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-20 20:43:23 +0000

    Thu, Apr 20, 2023, 4:43 PM -04:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 
 int count_letters(string text);
@@ -18,17 +19,17 @@ int count_letters(string text)
 {
     int count = 0;
     int length = strlen(text);
-    for(int i = 0;, i < length; i++)
+
+    for(int i = 0; i < length; i++)
         if(isupper(text[i]))
     {
-        count += text[i]
+        count += text[i];
     }
         else if(islower(text[i]))
-     {
+    {
+        count += text[i];
+    }
 
-        count += text[i]
-     }
-
-    return count_letters;
+    return count;
 
 }
