686de41e2a35c16e07942f2ed74e2e16df74cdf7
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-20 21:24:27 +0000

    Thu, Apr 20, 2023, 5:24 PM -04:00

--- readability/readability.c
+++ readability/readability.c
@@ -18,7 +18,7 @@ int main(void)
 int count_letters(string text)
 {
     int count = 0;
-    //int length = strlen(text);
+    int length = strlen(text);
 
     //for(int i = 0; i < length; i++)
         //if(isupper(text[i]))
