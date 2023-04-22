7c33d8f355b86956f07385d87ac09c25277a42d9
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-22 16:25:20 +0000

    Sat, Apr 22, 2023, 12:25 PM -04:00

--- readability/readability.c
+++ readability/readability.c
@@ -43,7 +43,7 @@ int count_words(string text)
     int length = strlen(text);
 
     for(int i = 0; i == length; i++)
-        if(text[i]) == (" ")
+        if((text[i]) == (' '))
         {
             words++;
         }
