4bb342644d5c595210ea0d8cd9f461d07b40f257
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-25 14:45:24 +0000

    Tue, Apr 25, 2023, 10:45 AM -04:00

--- readability/readability.c
+++ readability/readability.c
@@ -40,8 +40,7 @@ int main(void)
         printf()
     }
 
-    for(i = 0; i < grade_level; i++)
-        if(grade_level[i])
+   int grade[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 
 
     //printf("Index value is %i\n", grade_level); // not needed FR
