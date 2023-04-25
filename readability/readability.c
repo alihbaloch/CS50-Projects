c7a383c2ce127e207ba937be2acf9d1974727bc2
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-25 15:48:41 +0000

    Tue, Apr 25, 2023, 11:48 AM -04:00

--- readability/readability.c
+++ readability/readability.c
@@ -39,7 +39,7 @@ int main(void)
     }
    // else
     {
-   //     printf("Grade %i\n", grade_level);
+        printf("Grade %i\n", grade_level);
     }
 
    //int grade[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
