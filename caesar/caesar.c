53cd97e89998f266fe923a77bf67fafb1825d5a6
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-27 16:25:54 +0000

    Thu, Apr 27, 2023, 12:25 PM -04:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -26,7 +26,7 @@ bool only_digits(string s)
     char digits = '0', '1', '2', '3', '4', '5', '6', '7', '8', '9';
 
     for (int i = 0; i < length; i++)
-        if ((isdigit(s[i] >= 0)) || (isdigit(s[i] < 10)))
+        if (isdigit(s[i] == digits)) //|| (isdigit(s[i] < 10)))
         {
             //digits++;
 
