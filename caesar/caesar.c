6976d9d2b71785f64147c69270125b818dd073c7
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-04-30 20:12:57 +0000

    Sun, Apr 30, 2023, 4:12 PM -04:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -24,7 +24,7 @@ bool only_digits(string s)
 {
 
     char digits[] = [0,1,2,3,4,5,6,7,8,9];
-    int length = strlen(s);
+    int length = strlen(digits);
     int i;
 
     for (i = 0; i < length; i++)
