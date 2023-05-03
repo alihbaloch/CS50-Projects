7bb93a506a264bad1792ede8011ed7335e912b94
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-03 02:03:13 +0000

    Tue, May 2, 2023, 10:03 PM -04:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -28,7 +28,7 @@ int main(int argc, string argv[])
 
     string text = get_string("Plaintext: ");
 
-    int ci = (text[0] + argv[1]) % 26;
+    int ci = (text[0] + convert) % 26;
     printf("formula is %i\n", formula);
 
     //int t = strlen(text);
