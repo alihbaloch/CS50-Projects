18384c1f2d55274d9fdf8470660973632b723cc3
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-08 03:21:10 +0000

    Sun, May 7, 2023, 11:21 PM -04:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -71,7 +71,7 @@ bool vote(string name)
     for (int i = 0; i < candidate_count, i++)
     {
         if (strcmp(candidate_count[i].name, name) == 0)
-        candidates[i].vote++;
+        candidate[i].vote++;
         return true;
     }
 
