29a12c0e9cc4d5372bed6b1e66139053e9824da6
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-10 00:15:13 +0000

    Tue, May 9, 2023, 8:15 PM -04:00

---
+++ candidate.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cs50.h>
+
+typedef struct
+{
+    string name;
+    int votes;
+
+}
+candidate;
+
+int main(void);
+{
+
+
+}
+
+candidate get_candidate(string prompt, int)
