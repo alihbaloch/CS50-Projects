f1de3a13573d1474ea10e1056bab2160442ddf4f
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-13 16:08:53 +0000

    Sat, May 13, 2023, 12:08 PM -04:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -190,7 +190,7 @@ int find_min(void)
                 min_votes = candidates[i].votes;
             }
 
-    return 0;
+    return min_votes;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
