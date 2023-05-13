9ec0277469b2acf9dd849c8df56df5a6cc47e8c8
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-13 16:27:12 +0000

    Sat, May 13, 2023, 12:27 PM -04:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -185,12 +185,9 @@ int find_min(void)
 
     for (int i = 1; i < candidate_count; i++)
     {
-        if (candidates[i].eliminated)
+        if (candidates[i].eliminated && candidates[i].votes < min_votes)
         {
-            if (candidates[i].votes < min_votes)
-            {
-                min_votes = candidates[i].votes;
-            }
+            min_votes = candidates[i].votes;
         }
     }
     return min_votes;
