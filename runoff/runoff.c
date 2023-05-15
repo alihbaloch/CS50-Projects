0835ea3ad62f2d55c598cb1fd8137afff5296228
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-15 22:35:06 +0000

    Mon, May 15, 2023, 6:35 PM -04:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -125,7 +125,7 @@ int main(int argc, string argv[])
     return 0;
 }
 
- Record preference if vote is valid
+ //Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
     // TODO
