8f8ce3ec91580a912c5ca3fca5a078548aa06233
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-17 04:15:20 +0000

    Wed, May 17, 2023, 12:15 AM -04:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -201,15 +201,12 @@ bool is_tie(int min)
         if (!candidates[i].eliminated && candidates[i].votes == min)
         {
             //min = candidates[i].votes;
-            candidates[i].name = true;
+            //candidates[i].name = true;
             //is_a_tie = true;
             return true;
         }
-        else
-        {
-            return false;
-        }
 
+    return false;
 }
 
 // Eliminate the candidate (or candidates) in last place
