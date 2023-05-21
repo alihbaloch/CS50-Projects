c326a406e44baa09d35d4f9f8dbef0b09f564b91
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-21 11:53:22 +0000

    Sun, May 21, 2023, 7:53 AM -04:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -30,36 +30,28 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            //if (image[i][j].rgbtGreen > 255 || image[i][j].rgbtBlue > 255 || image[i][j].rgbtRed > 255)
+           sepiaGreen = round((0.349 * image[i][j].rgbtRed) + (0.686 * image[i][j].rgbtGreen) + (0.168 * image[i][j].rgbtBlue));
+           sepiaBlue = round((0.272 * image[i][j].rgbtRed) + (0.534 * image[i][j].rgbtGreen) + (0.131 * image[i][j].rgbtBlue));
+           sepiaRed = round((0.393 * image[i][j].rgbtRed) + (0.769 * image[i][j].rgbtGreen) + (0.189 * image[i][j].rgbtBlue));
+
+            if (sepiaGreen > 255)
             {
-                //image[i][j].rgbtGreen = 255 && image[i][j].rgbtBlue = 255 && image[i][j].rgbtRed = 255;
-
-                sepiaGreen = round((0.349 * image[i][j].rgbtRed) + (0.686 * image[i][j].rgbtGreen) + (0.168 * image[i][j].rgbtBlue));
-                sepiaBlue = round((0.272 * image[i][j].rgbtRed) + (0.534 * image[i][j].rgbtGreen) + (0.131 * image[i][j].rgbtBlue));
-                sepiaRed = round((0.393 * image[i][j].rgbtRed) + (0.769 * image[i][j].rgbtGreen) + (0.189 * image[i][j].rgbtBlue));
-
-                if (sepiaGreen > 255)
-                {
-                    sepiaGreen = 255;
-                }
-                if (sepiaBlue > 255)
-                {
-                    sepiaBlue = 255;
-                }
-                if (sepiaRed > 255)
-                {
-                    sepiaRed = 255;
-                }
-                else
-                {
-                    image[i][j].rgbtGreen = sepiaGreen;
-                    image[i][j].rgbtBlue = sepiaBlue;
-                    image[i][j].rgbtRed = sepiaRed;
-                }
-
+                sepiaGreen = 255;
             }
-        }
+            if (sepiaBlue > 255)
+            {
+                sepiaBlue = 255;
+            }
+            if (sepiaRed > 255)
+            {
+                sepiaRed = 255;
+            }
 
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
+            image[i][j].rgbtRed = sepiaRed;
+
+        }
     }
 
     return;
