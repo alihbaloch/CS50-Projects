117378978db4ce1ef772a1a770e61763c9617ed9
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-21 04:13:27 +0000

    Sun, May 21, 2023, 12:13 AM -04:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -6,7 +6,7 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
-            if {image[i][j],rgbtred == 0x00 && image[i][j].rgbtblue == 0x00 && image[i][j].rgbtgreen == 0x00}
+            if (image[i][j].rgbtred == 0x00 && image[i][j].rgbtblue == 0x00 && image[i][j].rgbtgreen == 0x00)
             {
                 image[i][j].rgbtred = 0xFF;
             }
