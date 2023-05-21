bd4fe1e43bc40e487dc3f352ac8ba53caca45e59
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-21 12:40:03 +0000

    Sun, May 21, 2023, 8:40 AM -04:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -59,7 +59,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
 
-            image[i][j].rgbt
+            swap(&i, &j);
 
         }
     }
