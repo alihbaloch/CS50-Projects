47f3f148daeb33d450e96bc3f2accdbecda29515
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-21 08:45:32 +0000

    Sun, May 21, 2023, 4:45 AM -04:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,6 +3,8 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    int average = rgbRed + rgbBlue + rgbGreen / 3;
+
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
