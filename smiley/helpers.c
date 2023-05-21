6d5fb8602df0fc627b098938db7840a8151b1af2
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-21 04:09:34 +0000

    Sun, May 21, 2023, 12:09 AM -04:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -3,4 +3,12 @@
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+            if {rgbtred image [i][j] == 0.00 && rgbtblue image [i][j] == 0.00 && rgbtgreen image [i][j] == 0.00}
+            {
+                rgbtred image [i][j] == 0.FF;
+            }
+    }
 }
