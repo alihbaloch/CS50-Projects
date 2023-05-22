6a7912835c606c0ceaf5a2bb6e0617cf9c978fa5
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-22 10:29:55 +0000

    Mon, May 22, 2023, 6:29 AM -04:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -55,17 +55,16 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
 
-    int temp = a;
-    int b = a
-    temp = b
-
+    int temp = *a;
+    int *a = *b;
+    int *b = temp;
 
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
 
-            swap(&i, &j);
+            swap(&height, &width);
 
         }
     }
