2162fa9c1074ada8917c09369a2346297da8d4c4
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-22 12:57:19 +0000

    Mon, May 22, 2023, 8:57 AM -04:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -84,6 +84,8 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    int average = 
+
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
