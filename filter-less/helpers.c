d216018a5a4d5870ff0e9a4e01bad769dd2517e0
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-23 16:06:13 +0000

    Tue, May 23, 2023, 12:06 PM -04:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -139,9 +139,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            image[i][j].rgbtRed = image[i][j].rgbtRed;
-            image[i][j].rgbtBlue = image[i][j].rgbtBlue;
-            image[i][j].rgbtGreen = image[i][j].rgbtGreen;
+            image[i][j].rgbtRed = copy[i][j].rgbtRed;
+            image[i][j].rgbtBlue = copy[i][j].rgbtBlue;
+            image[i][j].rgbtGreen = copy[i][j].rgbtGreen;
         }
     }
 
