dffa7bbac38278bad7abda754a57e4bdf111f357
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-23 12:58:14 +0000

    Tue, May 23, 2023, 8:58 AM -04:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -98,7 +98,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            
+            for (int p1 = -)
         }
     }
     }
