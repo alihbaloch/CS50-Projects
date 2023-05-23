1078a193dcaa27672a3199e9d90d45ace062bd06
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-23 14:35:48 +0000

    Tue, May 23, 2023, 10:35 AM -04:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -89,6 +89,8 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     int sumblue = 0;
     int sumgreen = 0;
 
+    // copy image pixel by pixel
+
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
