0a1f9dac34a546d3e8a0cfdeae1a01f4412e7d1d
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-26 09:39:11 +0000

    Fri, May 26, 2023, 5:39 AM -04:00

--- recover/recover.c
+++ recover/recover.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
 
     fread(buffer, 1, 4, jpeg);
 
-    uint8_t signature = {0xff, 0xd8, 0xff};
+    uint8_t signature[] = {0xff, 0xd8, 0xff};
 
     for (int i = 0; i < 3; i++)
     {
