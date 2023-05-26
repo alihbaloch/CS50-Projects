61a2b47ced3a969b89660910e5a7b8e2e1c6b8b5
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-26 13:13:54 +0000

    Fri, May 26, 2023, 9:13 AM -04:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,7 +3,6 @@
 #include <stdint.h>
 
 #define BLOCK_SIZE 512
-#define signature [] = {buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0};
 
 int main(int argc, char *argv[])
 {
@@ -38,9 +37,9 @@ int main(int argc, char *argv[])
     {
         // Check for signature
 
-        if (signature[])
+        if ({buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0};)
         {
-            printf("True\n");
+            
         }
 
     }
