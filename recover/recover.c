113e41c1f96366237e14cc6bc78b91c341f19c54
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-26 09:50:56 +0000

    Fri, May 26, 2023, 5:50 AM -04:00

--- recover/recover.c
+++ recover/recover.c
@@ -36,10 +36,10 @@ int main(int argc, char *argv[])
 
     for (int i = 0; i < 4; i++)
     {
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && buffer[3]  )
+        if (!(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0))
         {
-            printf("Values are different\n");
-            return 0;
+            printf("Values are not the same\n");
+            return 1;
         }
 
     }
