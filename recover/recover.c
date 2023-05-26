9ac8c3aab64b0ec329c87e606b0177235b6a10bf
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-26 12:47:35 +0000

    Fri, May 26, 2023, 8:47 AM -04:00

--- recover/recover.c
+++ recover/recover.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
 
     while (fread(buffer, 1, BLOCK_SIZE, jpeg) == BLOCK_SIZE)
     {
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < BLOCK_SIZE; i++)
         {
             if ((buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0))
             {
