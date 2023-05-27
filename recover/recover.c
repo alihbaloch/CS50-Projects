5784073fa16d720322952d29c35598cbb266f28e
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-27 13:22:30 +0000

    Sat, May 27, 2023, 9:22 AM -04:00

--- recover/recover.c
+++ recover/recover.c
@@ -37,9 +37,9 @@ int main(int argc, char *argv[])
     {
         // Check for signature
 
-        if ({buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0};)
+        if ({buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0});
         {
-            
+
         }
 
     }
