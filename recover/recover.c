0d2cd2a1ac78f7f3d7ff76224eb167fc134dd449
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-27 13:27:49 +0000

    Sat, May 27, 2023, 9:27 AM -04:00

--- recover/recover.c
+++ recover/recover.c
@@ -39,7 +39,8 @@ int main(int argc, char *argv[])
 
         if ({buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0});
         {
-            sprintf(")
+            sprintf(buffer, "%03i.jpg", 1);
+            
         }
 
     }
