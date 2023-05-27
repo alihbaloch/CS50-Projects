5888762fd6ed3362257d3a3b35e5a46f7bd7995f
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-27 14:12:42 +0000

    Sat, May 27, 2023, 10:12 AM -04:00

--- recover/recover.c
+++ recover/recover.c
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             //char *image_1 = NULL;
-            sprintf(image_1, "%03i.jpg", 1);
+            sprintf(image_1, "%03i.jpg", 0);
             FILE *IMAGE = fopen(image_1, "w");
             fwrite(&buffer, 1, BLOCK_SIZE, IMAGE);
 
