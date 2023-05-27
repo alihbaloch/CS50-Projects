868c209d6aa20f2bb0e63bb8e6e25940e2e3e613
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-27 16:54:36 +0000

    Sat, May 27, 2023, 12:54 PM -04:00

--- recover/recover.c
+++ recover/recover.c
@@ -66,6 +66,8 @@ int main(int argc, char *argv[])
             {
                 fclose(IMAGE);
                 fclose(jpeg);
+                free(buffer);
+                free(image);
             }
 
     }
