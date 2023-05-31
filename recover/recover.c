950c053d07af55e26e7dc2b9ee5d722541c33071
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-05-31 14:33:45 +0000

    Wed, May 31, 2023, 10:33 AM -04:00

--- recover/recover.c
+++ recover/recover.c
@@ -21,6 +21,7 @@ int main(int argc, char *argv[])
     // Open file using fopen in read mode
 
     FILE *JPEG = fopen(argv[1], "r");
+    fclose(argv[1]);
 
     // If file cannot be opened print error message
 
