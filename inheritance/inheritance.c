9cf829e86a0dfb01cac13ac1814934e0c388c972
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-06-05 22:49:55 +0000

    Mon, Jun 5, 2023, 6:49 PM -04:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -80,7 +80,7 @@ person *create_family(int generations)
     }
 
     // TODO: Return newly created person
-    return p;
+    return p;  // set to NULL if doesnt work
 }
 
 // Free `p` and all ancestors of `p`.
