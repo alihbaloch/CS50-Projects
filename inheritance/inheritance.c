7ffbb66097d8dedfda471a7de8a03837406782e6
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-06-05 23:01:37 +0000

    Mon, Jun 5, 2023, 7:01 PM -04:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -96,7 +96,8 @@ void free_family(person *p)
 
     // TODO: Free parents recursively
 
-    free(p -> parents[0] = p -> parents[1]);
+    free_family(p -> parents[0]);
+    free_family(p -> parents[1]);
 
     // TODO: Free child
 
