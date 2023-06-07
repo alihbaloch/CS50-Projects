02d97afa902620471986054c7c78cfdcb1a97a27
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-06-07 21:22:14 +0000

    Wed, Jun 7, 2023, 5:22 PM -04:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -52,7 +52,8 @@ person *create_family(int generations)
 
         // TODO: Set parent pointers for current person
 
-        p -> parents[0] = p -> parents[1];
+        p -> parents[0] = parents [0];
+        p -> parents[1] = parents [1];
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
 
