408e0d707c990eaad8d1de81253917ce4a5a92a9
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-06-15 22:38:09 +0000

    Thu, Jun 15, 2023, 6:38 PM -04:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -23,7 +23,7 @@ typedef struct node
 node;
 
 // TODO: Choose number of buckets in hash table
-const unsigned int N = 204415;
+const unsigned int N = 204415 + 17756;
 
 // Hash table
 node *table[N];
