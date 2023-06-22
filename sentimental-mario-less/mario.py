2a63c41fdf174d49af2ea5f7a2baf3d2041aef16
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-06-22 03:12:13 +0000

    Wed, Jun 21, 2023, 11:12 PM -04:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,2 +1,11 @@
 # Mario pyramid program
 
+number = int(input("Enter Pyramid Height: " ))
+
+for i in (range: 8):
+    if i > 8 or i < 1:
+        print("invalid height")
+        break
+    print("#")
+    or j in (range: 8)
+            print("#")
