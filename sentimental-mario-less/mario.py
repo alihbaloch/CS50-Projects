d621e4e75e0f61ed2794db5fa906ad3c3a146378
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-06-22 05:24:30 +0000

    Thu, Jun 22, 2023, 1:24 AM -04:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -3,7 +3,10 @@
 def main():
     height = Pyramid_Height()
     for i in range(height):
-        for space in range(i)
+        for space in range(i):
+            if space >= i:
+                space - 1
+                print(" ")
         for j in range(i):
             if j >= 0:
                 j - 1
