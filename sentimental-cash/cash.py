435e545671ec7cc102a33b39bbf079c0cd637d21
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-06-23 06:01:04 +0000

    Fri, Jun 23, 2023, 2:01 AM -04:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,1 +1,20 @@
-# TODO
+# Cash program
+
+from cs50 import get_float
+
+
+
+def main():
+
+    dollar_amount = get_dollars()
+
+
+
+def get_dollars():
+
+    while True:
+        dollars = get_float("Input number of dollars: ")
+        if dollars > 0:
+            return dollars
+
+main()
