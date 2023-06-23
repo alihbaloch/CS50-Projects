a2ae3b8e83b09838e80082e6bc4731ed9ca85383
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-06-23 08:09:56 +0000

    Fri, Jun 23, 2023, 4:09 AM -04:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -5,21 +5,28 @@ from cs50 import get_float
 
 def main():
 
+    # convert dollar amount to cents
     cents = int(get_dollars() * 100)
 
+    # calculate quarters
     quarter_amount = cents // 25
     cents = cents % 25
 
+    # calculate dimes
     dime_amount = cents // 10
     cents = cents % 10
 
+    # calculate nickels
     nickel_amount = cents // 5
     cents = cents % 5
 
+    # calculate pennies
     penny_amount = cents // 1
 
+    #  calculate coins
     coins = quarter_amount + dime_amount + nickel_amount + penny_amount
 
+    # print change owed
     print(f"change owed: ${coins}")
 
 
