742d98b5c6d9897a89a168fad8821a4eb54d3b07
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-06-23 14:42:42 +0000

    Fri, Jun 23, 2023, 10:42 AM -04:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -10,9 +10,10 @@ def get_text()
     grade_level = 0.0588 * L - 0.296 * S - 15.8
 
     text = get_string("Enter text: ")
-    if grade_level >
+    if grade_level < 1:
+        print("Before Grade 1")
+    elif grade_level >= 16:
+        print("Grade 16+")
+    else
+        print(f"Grade, round{grade_level}")
 
-
-
-
-def crea
