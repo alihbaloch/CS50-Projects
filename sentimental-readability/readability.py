7f5c9424f3daaa729066e369d88c6031c5e44723
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-06-26 22:49:51 +0000

    Mon, Jun 26, 2023, 6:49 PM -04:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -3,29 +3,11 @@
 from cs50 import get_string
 
 
-grade_level = 0.0588 * L - 0.296 * S - 15.8
+main()
 
-text = get_string("Enter text: ")
+    grade_level = 0.0588 * L - 0.296 * S - 15.8
 
-letter_count = 0
-word_count = 0
-sentence_count = 0
-letter_length = len(text)
-
-for letters in range(letter_length):
-    if letters = letters.lower() or letters.upper():
-        letter_count += 1
-
-for words in range(letter_length)
-    if words = " ":
-        word_count += 1
-
-for sentences in range(letter_length)
-    if sentences = "." or "!" or "?":
-        sentence_count += 1
-
-
-
+    text = get_string("Enter text: ")
 
     #if grade_level < 1:
     #    print("Before Grade 1")
@@ -33,4 +15,27 @@ for sentences in range(letter_length)
     #    print("Grade 16+")
     #else:
      #   print(f"Grade, round{grade_level}")
+
+
+def calculations()
+
+    letter_count = 0
+    word_count = 0
+    sentence_count = 0
+    letter_length = len(text)
+
+
+    for letters in range(letter_length):
+        if letters = letters.lower() or letters.upper():
+            letter_count += 1
+
+    for words in range(letter_length)
+        if words = " ":
+            word_count += 1
+
+    for sentences in range(letter_length)
+        if sentences = "." or "!" or "?":
+            sentence_count += 1
+
+main()
 
