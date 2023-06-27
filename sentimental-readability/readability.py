5069388c391bedb181e5e925e3652c78475c3cf0
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-06-27 06:48:45 +0000

    Tue, Jun 27, 2023, 2:48 AM -04:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -30,14 +30,14 @@ for i in text:
     #L = (letter_count / word_count) * 100
     #S = (sentence_count / word_count) * 100
 
-    grade_level = 0.0588 * (letter_count/word_count * 100) - 0.296 * (sentence_count/ word_count *100) - 15.8
+grade_level = 0.0588 * (letter_count/word_count * 100) - 0.296 * (sentence_count/ word_count *100) - 15.8
 
-    if grade_level < 1:
-        print("Before Grade 1")
-    elif grade_level >= 16:
-        print("Grade 16+")
-    else:
-        print("Grade ", round(grade_level))
+if grade_level < 1:
+    print("Before Grade 1")
+elif grade_level >= 16:
+    print("Grade 16+")
+else:
+    print("Grade ", round(grade_level))
 
 
 #def calculations():
