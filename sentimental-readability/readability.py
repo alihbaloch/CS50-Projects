f5d9317f8220fd0e448da27a7cf31906b4b9d631
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-06-27 05:05:36 +0000

    Tue, Jun 27, 2023, 1:05 AM -04:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -5,27 +5,6 @@ from cs50 import get_string
 
 def main():
 
-    letters = calculations(letter_count)
-    words = calculations(word_count)
-    sentences = calculations(sentence_count)
-
-    L = ((float) letter_count / word_count) * 100
-    S = ((float) sentence_count / word_count) * 100
-
-    grade_level = 0.0588 * L - 0.296 * S - 15.8
-
-    text = get_string("Enter text: ")
-
-    #if grade_level < 1:
-    #    print("Before Grade 1")
-    #elif grade_level >= 16:
-    #    print("Grade 16+")
-    #else:
-     #   print(f"Grade, round{grade_level}")
-
-
-def calculations():
-
     letter_count = 0
     word_count = 0
     sentence_count = 0
@@ -44,5 +23,25 @@ def calculations():
         if sentences = (".") or ("!") or ("?"):
             sentence_count += 1
 
-main()
+
+    L = ((float) letter_count / word_count) * 100
+    S = ((float) sentence_count / word_count) * 100
+
+    grade_level = 0.0588 * L - 0.296 * S - 15.8
+
+    text = get_string("Enter text: ")
+
+    #if grade_level < 1:
+    #    print("Before Grade 1")
+    #elif grade_level >= 16:
+    #    print("Grade 16+")
+    #else:
+     #   print(f"Grade, round{grade_level}")
+
+
+#def calculations():
+
+
+
+#main()
 
