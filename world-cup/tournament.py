1fb6f838023a2f5563e8e21d79cabe5f91424ce0
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-06-28 10:15:33 +0000

    Wed, Jun 28, 2023, 6:15 AM -04:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -16,7 +16,7 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
-    with open(2018m.csv) as file:
+    with open("2018m.csv") as file:
         reader = csv.DictReader(int(file))
 
     counts = {}
