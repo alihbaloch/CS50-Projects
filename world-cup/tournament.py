1885304960cbd80f772daae2e0f1e433d0559c3f
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-06-29 07:26:49 +0000

    Thu, Jun 29, 2023, 3:26 AM -04:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -69,7 +69,7 @@ def simulate_tournament(teams):
 
     while len(teams) > 1:
         teams = simulate_round(teams)
-    return teams[0]["teams"]
+    return teams[0]["team"]
 
 
 if __name__ == "__main__":
