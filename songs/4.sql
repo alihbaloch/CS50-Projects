c9dace2154f96c6991337d1bb182f28b901c1937
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-05 08:50:36 +0000

    Wed, Jul 5, 2023, 4:50 AM -04:00

--- songs/4.sql
+++ songs/4.sql
@@ -1,4 +1,4 @@
 -- names of any songs that have danceability, energy, and valence greater than 0.75
 
 SELECT name FROM songs
-    WHERE danceability AND Energy AND valence > 0.75;
+    WHERE danceability AND Energy AND valence > 0.75;  --CHECK 
