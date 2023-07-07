bc0fd0af9e1352ca2b9233cef8f359d066ade31d
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-07 12:59:35 +0000

    Fri, Jul 7, 2023, 8:59 AM -04:00

--- movies/9.sql
+++ movies/9.sql
@@ -2,5 +2,5 @@
 
 SELECT name FROM people
     WHERE id IN (SELECT person_id IN stars
-    WHERE movie_id (SELECT id FROM movies WHERE year = 2004))
+    WHERE movie_id = (SELECT id FROM movies WHERE year = 2004))
     ORDER BY birth;
