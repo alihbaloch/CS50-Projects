30184ec3517693ef6b204ef5649b0ab334b54542
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-07 13:11:43 +0000

    Fri, Jul 7, 2023, 9:11 AM -04:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,6 +1,6 @@
 --  Names of all people who starred in a movie released in 2004, ordered by birth year
 
-SELECT name
+SELECT name, 
 FROM people
     WHERE id IN (SELECT person_id FROM stars
     WHERE movie_id IN (SELECT id FROM movies
