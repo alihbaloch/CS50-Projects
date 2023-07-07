22be1107d210bdf9024a18073d69e1b7f541e85a
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-07 09:06:08 +0000

    Fri, Jul 7, 2023, 5:06 AM -04:00

--- movies/3.sql
+++ movies/3.sql
@@ -1,3 +1,3 @@
 -- Titles of all movies with a release date on or after 2018, in alphabetical order.
 
-SELECT title FROM movies WHERE year >= 2018;
+SELECT title FROM movies WHERE year >= 2018 ORDER BY title ASC;
