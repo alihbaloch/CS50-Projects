6e673b251f306bc9aad5b477f6a22ef7b155e304
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-07 11:37:36 +0000

    Fri, Jul 7, 2023, 7:37 AM -04:00

--- movies/8.sql
+++ movies/8.sql
@@ -6,7 +6,7 @@ SELECT name AS Stars_in_Toy_Story FROM people
     WHERE title = 'Toy Story'));
 
 
-SELECT people.name, stars.person_id
+SELECT people.name
     FROM people
     JOIN stars ON people.id = stars.person_id
     JOIN movies ON stars.movie_id = movies.id
