b54e7dec3e1eb74369b1b7af37c0d899723105f7
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-07 10:31:11 +0000

    Fri, Jul 7, 2023, 6:31 AM -04:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,4 +1,5 @@
 -- All movies released in 2010 and their ratings, in descending order by rating. For movies with the same rating, order them alphabetically by title.
 
 SELECT movies.title, ratings.rating
-    FROM movies JOIN ratings ON movies.id = ratings.movies_id;
+    FROM movies
+    JOIN ratings ON movies.id = ratings.movies_id;
