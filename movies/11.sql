d0cb0c7288917369506680eac1f93c94bb35f8c8
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-08 14:03:12 +0000

    Sat, Jul 8, 2023, 10:03 AM -04:00

--- movies/11.sql
+++ movies/11.sql
@@ -20,3 +20,16 @@ LIMIT 5;
 
 
 
+SELECT movies.title
+FROM movies
+    JOIN stars
+    ON movies.id = stars.movie_id
+    JOIN people
+    ON stars.person_id = people.id
+    JOIN ratings
+    ON stars.movie_id = ratings.movie_id
+WHERE people.name = 'Chadwick Boseman'
+ORDER BY ratings.rating DESC
+LIMIT 5;
+
+
