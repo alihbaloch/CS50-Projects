31d5569c3834b6087541c5942d7ade9cce04ace4
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-08 16:24:33 +0000

    Sat, Jul 8, 2023, 12:24 PM -04:00

--- movies/5.sql
+++ movies/5.sql
@@ -1,6 +1,6 @@
 -- Titles and release years of all Harry Potter movies, in chronological order
 
-SELECT COUNT(title), year
+SELECT title, year
 FROM movies
 WHERE title LIKE 'Harry Potter%'
 ORDER BY year ASC;
