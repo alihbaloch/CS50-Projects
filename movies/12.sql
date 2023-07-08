fc2bdb39f489375eee8c132fbca90f64fea22e33
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-08 16:37:50 +0000

    Sat, Jul 8, 2023, 12:37 PM -04:00

--- movies/12.sql
+++ movies/12.sql
@@ -2,12 +2,12 @@
 
 SELECT COUNT(movies.title)
 FROM movies
-    JOIN stars AS s1
-    ON movies.id = s1.movie_id
-    JOIN stars AS s2
-    ON movies.id = s2.movie_id
-    JOIN people AS p1
-    ON s1.person_id = p1.id
-    JOIN people AS p2
-    ON s2.person_id = p2.id
-WHERE p1.name = 'Johnny Depp' AND p2.name = 'Helena Bonham Carter';
+    JOIN stars AS jd
+    ON movies.id = jd.movie_id
+    JOIN stars AS hc
+    ON movies.id = hc.movie_id
+    JOIN people AS p_jd
+    ON jd.person_id = p_jd.id
+    JOIN people AS p_hc
+    ON hc.person_id = p_hc.id
+WHERE jd.name = 'Johnny Depp' AND hc.name = 'Helena Bonham Carter';
