fa18c38da7641d5d0611f0dc457e3194075cf7bf
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-08 11:11:17 +0000

    Sat, Jul 8, 2023, 7:11 AM -04:00

--- movies/6.sql
+++ movies/6.sql
@@ -1,3 +1,5 @@
 -- Average rating of all movies released in 2012
 
-SELECT AVG(rating) FROM ratings WHERE movie_id IN (SELECT id FROM movies WHERE year = 2012);
+SELECT AVG(rating)
+    FROM ratings
+    WHERE movie_id IN (SELECT id FROM movies WHERE year = 2012);
