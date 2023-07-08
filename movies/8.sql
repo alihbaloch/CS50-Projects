890226ebd40dd9af912d400fbdb2045c741caa5c
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-08 11:00:26 +0000

    Sat, Jul 8, 2023, 7:00 AM -04:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,6 +1,6 @@
 -- Names of all people who starred in Toy Story
 
-SELECT name AS Stars_in_Toy_Story FROM people
+SELECT name AS stars_in_toy_story FROM people
     WHERE id IN (SELECT person_id FROM stars
     WHERE movie_id IN (SELECT id FROM movies
     WHERE title = 'Toy Story'));
