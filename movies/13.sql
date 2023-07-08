dc25085f1b45cbc527881d88e67ce19eb2fd8f18
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-08 15:52:31 +0000

    Sat, Jul 8, 2023, 11:52 AM -04:00

--- movies/13.sql
+++ movies/13.sql
@@ -4,4 +4,5 @@ SELECT people.name
 FROM people
     JOIN stars ON people.id = stars.person_id
     JOIN movies ON stars.movie_id = movies.id;
+    JOIN stars ON
 --WHERE people.name = 'Kevin Bacon' AND people.birth = 1958;
