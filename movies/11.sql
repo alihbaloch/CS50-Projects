08fdd6a8a26560b02324a42f4188bbd455ddb055
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-08 12:23:44 +0000

    Sat, Jul 8, 2023, 8:23 AM -04:00

--- movies/11.sql
+++ movies/11.sql
@@ -6,7 +6,8 @@ WHERE id IN (
     SELECT movie_id
     FROM stars
     WHERE person_id IN (
-        SELECT name FROM people
+        SELECT name
+        FROM people
         WHERE name = 'Chadwick Boseman'
     )
 ORDER BY ratings: TOP 5;
