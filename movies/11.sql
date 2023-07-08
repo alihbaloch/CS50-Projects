41cd2df52557946255fdd78c4fe4946598b01d0c
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-08 13:54:32 +0000

    Sat, Jul 8, 2023, 9:54 AM -04:00

--- movies/11.sql
+++ movies/11.sql
@@ -4,12 +4,10 @@ SELECT movies.title
 FROM movies
     JOIN stars
     ON movies.id = stars.movie_id
-    -- FROM stars
-        JOIN people
     JOIN people
     ON stars.person_id = people.id
     JOIN ratings
-    ON stars.movie_id = ratings.movie_id
+    ON movies.id = ratings.movie_id
 WHERE people.name = 'Chadwick Boseman'
 ORDER BY ratings.rating ASC
 LIMIT 5;
