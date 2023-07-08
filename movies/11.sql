239eee10e87866e7fb120a8460550821b86fa52a
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-08 13:26:13 +0000

    Sat, Jul 8, 2023, 9:26 AM -04:00

--- movies/11.sql
+++ movies/11.sql
@@ -4,11 +4,12 @@ SELECT movies.title
 FROM movies
     JOIN stars
     ON movies.id = stars.movie_id
+    JOIN people
+    ON stars.person_id = people.id
     JOIN ratings
     ON stars.movie_id = ratings.movie_id
 
-
-WHERE stars.person_id = 'Ch
+WHERE stars.person = 'Ch
 
 
 
