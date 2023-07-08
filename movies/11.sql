b64704eb3dd6c8be2a043d58f4d9e8b3af076b8e
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-08 14:09:53 +0000

    Sat, Jul 8, 2023, 10:09 AM -04:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,25 +1,5 @@
 -- List the titles of the five highest rated movies (in order) that Chadwick Boseman starred in, starting with the highest rated
 
-/*SELECT movies.title
-FROM movies
-    JOIN stars
-    ON movies.id = stars.movie_id
-    JOIN people
-    ON movies.id = people.id
-    JOIN ratings
-    ON movies.id = ratings.movie_id
-WHERE people.name = 'Chadwick Boseman'
-ORDER BY ratings.rating DESC
-LIMIT 5; */
-
-
-
-
-
-
-
-
-
 SELECT movies.title
 FROM movies
     JOIN stars
