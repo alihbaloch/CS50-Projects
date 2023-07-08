f81477e5c6d5330c36894d7d12ee3f568b81535d
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-08 11:07:42 +0000

    Sat, Jul 8, 2023, 7:07 AM -04:00

--- movies/7.sql
+++ movies/7.sql
@@ -2,6 +2,7 @@
 
 SELECT movies.title, ratings.rating
     FROM movies
-    JOIN ratings ON movies.id = ratings.movie_id
+        JOIN ratings
+        ON movies.id = ratings.movie_id
     WHERE movies.year = 2010
     ORDER BY ratings.rating DESC, movies.title ASC;
