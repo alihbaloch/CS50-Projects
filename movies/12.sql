c7e8d46fac85b72724340d294bc106391fb9086a
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-08 14:37:11 +0000

    Sat, Jul 8, 2023, 10:37 AM -04:00

--- movies/12.sql
+++ movies/12.sql
@@ -5,9 +5,9 @@ FROM movies
     JOIN stars AS star_jd
     ON movies.id = star_jd.movie_id
     JOIN stars AS star_hbc
-    ON movies.id = star_jd.movie_id
-    JOIN people
-    ON stars.person_id = people.id
+    ON movies.id = star_hbc.movie_id
+    JOIN people AS p_jd
+    ON star_jd.person_id = people.id
 WHERE people.name = 'Johnny Depp'
 
 
