35582a6e4b29f31a424d02d64eac6bdf8c69e28a
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-08 15:30:58 +0000

    Sat, Jul 8, 2023, 11:30 AM -04:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,3 +1,6 @@
 -- List the names of all people who starred in a movie in which Kevin Bacon also starred
 
-SELECT people.name 
+SELECT people.name
+FROM people
+    JOIN stars ON people.id = stars.person_id
+    JOIN movies ON stars.movie_id = movies.id
