5c51005567b14d0c0f8866415d82cb29e18a1255
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-08 14:35:08 +0000

    Sat, Jul 8, 2023, 10:35 AM -04:00

--- movies/12.sql
+++ movies/12.sql
@@ -2,9 +2,14 @@
 
 SELECT movies.title
 FROM movies
-    JOIN stars
-    ON movies.id = stars.movie_id
+    JOIN stars AS star_jd
+    ON movies.id = star_jd.movie_id
+    JOIN stars AS star_hbc
+    ON movies.id = star_jd.movie_id
     JOIN people
     ON stars.person_id = people.id
 WHERE people.name = 'Johnny Depp'
+
+
+
 AND people.name = 'Helena Bonham Carter';
