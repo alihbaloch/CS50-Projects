7955eec0b0723863ea895ac7dcb3f6ec43ab337b
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-10 22:47:21 +0000

    Mon, Jul 10, 2023, 6:47 PM -04:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -62,4 +62,4 @@ WHERE atm_transactions.day = 28
     AND atm_transactions.month = 7
     AND atm_transactions.atm_location = 'Leggett Street'
     AND bakery_security_logs.hour = 10
-    AND bakery_security_logs.minute BETWEEN 15 AND 35;
+    AND bakery_security_logs.minute BETWEEN 15 AND 25;
