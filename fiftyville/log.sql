2c7b53272f03550487476f8ec476049afdb4484a
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-10 21:56:47 +0000

    Mon, Jul 10, 2023, 5:56 PM -04:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -45,7 +45,6 @@ SELECT people.id, people.name, people.phone_number, people.passport_number, peop
 FROM people
     JOIN bank_accounts ON people.id = bank_accounts.person_id
     JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
-    JOIN bakery_security_logs ON
 WHERE atm_transactions.day = 28 AND atm_transactions.month = 7 AND atm_transactions.atm_location = 'Leggett Street';
 
 
