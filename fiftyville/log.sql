b8ed3feb47933b42a1d3b0cdcd46452336e096dd
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-10 23:12:15 +0000

    Mon, Jul 10, 2023, 7:12 PM -04:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -38,7 +38,7 @@ FROM people
     JOIN bank_accounts ON people.id = bank_accounts.person_id
     JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
     JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
-    JOIN people ON phone_calls.id = people.id
+    JOIN phone_calls ON phone_calls.id = people.id
 WHERE atm_transactions.day = 28
     AND atm_transactions.month = 7
     AND atm_transactions.atm_location = 'Leggett Street'
