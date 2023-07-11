5928ffe097cd361c19d3f17314bac8d6dcdbf598
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-11 18:58:06 +0000

    Tue, Jul 11, 2023, 2:58 PM -04:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -19,7 +19,6 @@ WHERE (
     According to Raymond: Thief talked to someone for 'LESS THAN 1 MINUTE'. Thief 'PLANNED TO LEAVE BY EARLIEST FLIGHT TOMORROW -> ON JULY 29'
                           person on the phone possibly bought the flight ticket
 
-                          TRY JOINING BAKERY + ATM_TRANSACTIONS + BANK_ACCOUNTS + PEOPLE + FLIGHTS
 */
 
 SELECT transcript, name, day, month
@@ -30,7 +29,7 @@ WHERE (
 );
 
 
-/* All individuals who left bakery parking lot between 10:15 - 10:25 am and withdrawed money from Leggett Street ATM on the day and month of the robbery */
+-- All individuals who left bakery parking lot between 10:15 - 10:25 am and withdrawed money from Leggett Street ATM on the day and month of the robbery
 
 
 SELECT people.id, people.name, people.phone_number, people.passport_number, people.license_plate, bakery_security_logs.activity, bakery_security_logs.hour, bakery_security_logs.minute
@@ -46,17 +45,19 @@ WHERE atm_transactions.day = 28
     AND bakery_security_logs.minute BETWEEN 15 AND 25;
 
 
-/* Based on the phone calls made in the time-period of the robbery, with durations of less than 60 seconds, 2 numbers match from the info we already have from the previous query.
-    2 people of interest:
-    Bruce (passport no: 5773159633) -> called (375) 555-8161 (potential accomplice)
-    Diana (passport no: 3592750733) -> called (725) 555-3243 (potential accomplice)
-*/
+-- All phone calls made with a duration of less than 60 seconds, on the day and month of the robbery
 
 SELECT caller, receiver
 FROM phone_calls
 WHERE phone_calls.duration < 60
     AND phone_calls.day = 28
     AND phone_calls.month = 7;
+
+ /* Based on the phone calls made in the time-period of the robbery, with durations of less than 60 seconds, 2 numbers match from the info we already have from the previous query.
+    2 people of interest:
+    Bruce (passport no: 5773159633) -> called (375) 555-8161 (potential accomplice)
+    Diana (passport no: 3592750733) -> called (725) 555-3243 (potential accomplice)
+*/
 
 
 
