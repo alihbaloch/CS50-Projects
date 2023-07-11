b894b681e7fca0c66f1d910cd649e150addf0126
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-11 17:57:41 +0000

    Tue, Jul 11, 2023, 1:57 PM -04:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -54,10 +54,10 @@ WHERE phone_calls.duration < 60
     AND phone_calls.day = 28;
 
 
-SELECT airports.id, airports.full_name, flights.orgin_airport_id, flights.destination_airport_id
+SELECT airports.id, airports.full_name, flights.orgin_airpor, flights.destination_airport_id
 FROM airports
     JOIN flights ON airports.id = flights.id
     JOIN passengers ON flights.id = passengers.flight_id
-WHERE flights.origin_airport_id =
+WHERE airports.city = 'Fiftyville'
     AND flights.day = 29
-    AND flights.month = 7
+    AND flights.month = 7;
