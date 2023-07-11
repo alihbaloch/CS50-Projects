89c9d173e43296b3c8ab9da872d8cddd5f937c93
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-11 19:31:49 +0000

    Tue, Jul 11, 2023, 3:31 PM -04:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -76,6 +76,7 @@ FROM passengers
 WHERE flights.origin_airport_id = 8
     AND flights.day = 29
     AND flights.month = 7
-    AND flights.year = 2021;
+    AND flights.year = 2021
+ORDER BY flights.hour DESC;
 
 
