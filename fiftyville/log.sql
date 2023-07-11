14a479845c091d6527861bdac33199acca3b21bb
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-07-11 18:59:55 +0000

    Tue, Jul 11, 2023, 2:59 PM -04:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,10 +1,6 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
-/*  Theft of duck took place at '10:15am'
-    3 witnesses interviewed who were present at the time
-    All of them mention the 'Bakery'
-    littering at 16:36 / probably irrelevant
-*/
+
 
 SELECT street, day, month, description
 FROM crime_scene_reports
@@ -14,12 +10,6 @@ WHERE (
     month = 7
 );
 
-/*  According to Ruth: thief left from bakery parking lot by car after 10 min of theft (check bakery security footage from '10:15 - 10:25 am')
-    According to Eugene: recognized thief -> thief withdrawed money from 'Leggett Street ATM'
-    According to Raymond: Thief talked to someone for 'LESS THAN 1 MINUTE'. Thief 'PLANNED TO LEAVE BY EARLIEST FLIGHT TOMORROW -> ON JULY 29'
-                          person on the phone possibly bought the flight ticket
-
-*/
 
 SELECT transcript, name, day, month
 FROM interviews
@@ -27,6 +17,24 @@ WHERE (
     day = 28 AND
     month = 7
 );
+
+
+
+/*
+    Theft of duck took place at '10:15am'
+    3 witnesses interviewed who were present at the time
+    All of them mention the 'Bakery'
+    littering at 16:36 / probably irrelevant
+
+    According to Ruth: thief left from bakery parking lot by car after 10 min of theft (check bakery security footage from '10:15 - 10:25 am')
+    According to Eugene: recognized thief -> thief withdrawed money from 'Leggett Street ATM'
+    According to Raymond: Thief talked to someone for 'LESS THAN 1 MINUTE'. Thief 'PLANNED TO LEAVE BY EARLIEST FLIGHT TOMORROW -> ON JULY 29'
+                          person on the phone possibly bought the flight ticket
+*/
+
+
+
+
 
 
 -- All individuals who left bakery parking lot between 10:15 - 10:25 am and withdrawed money from Leggett Street ATM on the day and month of the robbery
