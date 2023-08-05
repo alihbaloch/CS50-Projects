89717c6ac795b78893f637bba7e7b90ee7ba3657
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-08-05 21:33:59 +0000

    Sat, Aug 5, 2023, 5:33 PM -04:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -39,7 +39,7 @@ def index():
     else:
 
         # TODO: Display the entries in the database on index.html
-        birthday = db.execute("SELECT * FROM birthdays")
-        return render_template("index.html", birthdays=birthday)
+        birthdays = db.execute("SELECT * FROM birthdays")
+        return render_template("index.html", birthdays=birthdays)
 
 
