c70a1832ec2afc124e923997ee346c4a408a9470
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-08-05 21:36:09 +0000

    Sat, Aug 5, 2023, 5:36 PM -04:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -27,18 +27,23 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
+
+        # Access Form Data
         name = request.form.get("name")
         month = request.form.get("month")
         day = request.form.get("day")
 
-
+        # Insert data
         db.execute("INSERT * INTO birthdays (name, month, day) VALUES (?, ?, ?)", name, month, day)
 
+        # Go back to homepage
         return redirect("/")
 
     else:
 
         # TODO: Display the entries in the database on index.html
+
+        # Execute sql query
         birthdays = db.execute("SELECT * FROM birthdays")
         return render_template("index.html", birthdays=birthdays)
 
