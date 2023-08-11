7da801ed7c7cddcf5e124783378b2a66c96f95dd
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-08-11 23:07:54 +0000

    Fri, Aug 11, 2023, 7:07 PM -04:00

--- finance/app.py
+++ finance/app.py
@@ -110,8 +110,8 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-
-    return redirect("/")
+    if request.method=="POST":
+        return redirect("/")
 
 
     return apology("TODO")
