c198af7ccf5e65e394acbbc3a090f3d48ab4c27d
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-08-17 04:01:45 +0000

    Thu, Aug 17, 2023, 12:01 AM -04:00

--- finance/app.py
+++ finance/app.py
@@ -86,7 +86,6 @@ def buy():
 
         # Update the "transactions" table to record user's buying history
         db.execute("INSERT INTO transactions (user_id, symbol, shares, price, date) VALUES (?, ?, ?, ?, ?)", (user_id, stock_price, shares, stock_costs, date))
-                   #stock_price, shares, stock_costs, date)
 
         flash("Share(s) Bought")
 
