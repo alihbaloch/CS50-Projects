b7e18200525d16fb9f8c52ae4a6395561505fb11
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-08-18 06:16:02 +0000

    Fri, Aug 18, 2023, 2:16 AM -04:00

--- finance/app.py
+++ finance/app.py
@@ -248,13 +248,16 @@ def sell():
         shares = request.form.get("shares")
         symbol = request.form.get("symbol")
 
+        if not shares:
+            return apology("Please input number of shares", 403)
+        elif not symbol:
+                return apology("Please select a stock", 403)
+
         user_shares = db.execute("SELECT SUM(shares) FROM transactions WHERE user_id = ? AND symbol = ?", user_id, symbol)[0]["SUM(shares)"]
 
         try:
             shares = int(shares)
-            if not symbol:
-                return apology("Please select a stock", 403)
-            elif not shares or shares < 1:
+            if not shares or shares < 1:
                 return apology("Please input a positive number of shares", 403)
             elif shares > user_shares:
                 return apology("You do not have enough shares of this stock", 403)
