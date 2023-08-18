125309b9d5954510bb9fa992da4025308297b282
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-08-18 01:55:08 +0000

    Thu, Aug 17, 2023, 9:55 PM -04:00

--- finance/app.py
+++ finance/app.py
@@ -260,7 +260,9 @@ def sell():
         share_name = lookup(symbol)
         share_price = share_name["price"]
 
-         = db.execute("SELECT cash FROM users WHERE id = ?", user_id)
+        cash_value = db.execute("SELECT cash FROM users WHERE id = ?", user_id)
+        user_cash = cash_value[0]["cash"]
+
         update_cas
 
     else:
