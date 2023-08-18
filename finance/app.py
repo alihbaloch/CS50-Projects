0f30151cfccc1a1e32dbae12fa30e1545c34671e
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-08-18 01:53:10 +0000

    Thu, Aug 17, 2023, 9:53 PM -04:00

--- finance/app.py
+++ finance/app.py
@@ -260,6 +260,9 @@ def sell():
         share_name = lookup(symbol)
         share_price = share_name["price"]
 
+        user_cash = db.execute("SELECT cash FROM users WHERE id = ?", user_id)
+        update_cas
+
     else:
 
         symbols = db.execute("SELECT symbol FROM transactions WHERE user_id = ? GROUP BY symbol", user_id)
