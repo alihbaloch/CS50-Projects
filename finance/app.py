cbadd0235b308d8c53c3c0f6c7174df7c97a4c7f
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-08-18 06:47:22 +0000

    Fri, Aug 18, 2023, 2:47 AM -04:00

--- finance/app.py
+++ finance/app.py
@@ -262,7 +262,7 @@ def sell():
             elif int_shares > user_shares:
                 return apology("You do not have enough shares of this stock", 403)
         except ValueError:
-            return apology("Invalid input, 403")
+            pass
 
         stock_price = lookup(symbol)
         total_sold = stock_price["price"] * int_shares
