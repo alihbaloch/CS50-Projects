62097a4c54fe75f63007047fe3c4f6574f54f118
alihbaloch <128831552+alihbaloch@users.noreply.github.com>
2023-08-22 03:13:24 +0000

    Mon, Aug 21, 2023, 11:13 PM -04:00

--- finance/app.py
+++ finance/app.py
@@ -180,7 +180,7 @@ def login():
         # Redirect user to home page
         return redirect("/")
 
-    # User reached route via GET (as by clicking a link or via redirect)
+    # If user reached route via GET 
     else:
         return render_template("login.html")
 
